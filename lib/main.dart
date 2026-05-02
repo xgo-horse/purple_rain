87387ab2675d7b6067ac718c38e310d6709bd0ba
Michel Hoekstra <michelhoekstra@MacBook-Pro-de-Michel.local>
2026-05-02 12:06:52 -0300

    first iteration of the rain

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,111 +15,119 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: MyHomePage(size: MediaQuery.sizeOf(context)),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.size});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final Size size;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<RainParticle> particles = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+
+    for (var i = 0; i < 50; i++) {
+      particles.add(RainParticle.random(widget.size));
+    }
+    Timer.periodic(Duration(milliseconds: 10), (_) {
+      for (var particle in particles) {
+        if (particle.y >= 0.8) {
+          particle.randomize(widget.size);
+        } else {
+          particle.step();
+        }
+      }
+      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: CustomPaint(
+          size: widget.size,
+          painter: PurpleRainPainter(particles),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+}
+
+class PurpleRainPainter extends CustomPainter {
+  const PurpleRainPainter(this.particles);
+
+  final List<RainParticle> particles;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final grassPaint = Paint()..color = Colors.green;
+    canvas.drawRect(
+      Rect.fromPoints(
+        Offset(size.width, size.height),
+        Offset(0, size.height - size.height * 0.2),
+      ),
+      grassPaint,
+    );
+
+    for (var particle in particles) {
+      particle.draw(canvas, size);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class RainParticle {
+  double z;
+  double x;
+  double y;
+
+  RainParticle({required this.z, required this.x, required this.y});
+
+  factory RainParticle.random(Size canvasSize) {
+    final particle = RainParticle(z: 0, x: 0, y: 0);
+    particle.randomize(canvasSize);
+
+    return particle;
+  }
+
+  void randomize(Size canvasSize) {
+    z = Random().nextDouble() + 0.1;
+    x = Random().nextDouble();
+    y = -Random().nextDouble();
+  }
+
+  void draw(Canvas canvas, Size size) {
+    final particleOffset = Offset(size.width * x, size.height * y);
+
+    final paint = Paint()..color = Colors.purple;
+
+    canvas.drawRect(
+      Rect.fromCenter(
+        center: particleOffset,
+        width: 5 * z + 1,
+        height: 16 * z + 2,
+      ),
+      paint,
+    );
+  }
+
+  void step() {
+    y = y + 0.01 * z;
   }
 }
