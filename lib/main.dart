4175fbd4ecd1f245879657e7f61eacc2e2e8f05a
Michel Hoekstra <michelhoekstra@MacBook-Pro-de-Michel.local>
2026-05-02 12:07:33 -0300

    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Purple Rain',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: MyHomePage(size: MediaQuery.sizeOf(context)),
     );
